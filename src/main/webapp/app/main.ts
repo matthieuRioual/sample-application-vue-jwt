eebd7a8d86e2cd83e6af74fb2e16f12dcb448490
matthieulapatate <m.rioual83@gmail.com>
2022-04-21 15:04:25 +0200

    Add Styled Vue

--- src/main/webapp/app/main.ts
+++ src/main/webapp/app/main.ts
@@ -1,14 +1,9 @@
 import { createApp } from 'vue';
 import App from './common/primary/app/App.vue';
 import router from './router/router';
-import {createPinia} from 'pinia';
-import piniaPersist from 'pinia-plugin-persist'
 // jhipster-needle-main-ts-import
 
 const app = createApp(App);
 app.use(router);
-const pinia = createPinia();
-pinia.use(piniaPersist);
-app.use(pinia);
 // jhipster-needle-main-ts-provider
 app.mount('#app');
