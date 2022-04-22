301022f33d096e6e907d1ba5f1278a07d53246dd
matthieulapatate <m.rioual83@gmail.com>
2022-04-22 09:59:34 +0200

    Test User  and UserCredential class constructor

---
+++ src/test/javascript/spec/common/domain/User.spec.ts
@@ -0,0 +1,17 @@
+import { User } from '@/common/domain/User';
+import { UserCredentialsDTO } from '@/common/domain/User';
+
+describe('Initialization of User and UserCredentialDTO', () => {
+  it('User constructor', () => {
+    const testingUser = new User(
+      'Matthieu',
+      '1234567',
+      true,
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    );
+    expect(testingUser.username).toBe('Matthieu');
+    const UserCredential = new UserCredentialsDTO('username', 'password');
+    expect(UserCredential.username).toBe('username');
+    expect(UserCredential.password).toBe('password');
+  });
+});
