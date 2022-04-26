6d0647b773300ee3458c66d10ff01da48c899895
matthieulapatate <m.rioual83@gmail.com>
2022-04-26 10:24:28 +0200

    change return type of login function

--- src/main/webapp/app/common/domain/ConnectionService.ts
+++ src/main/webapp/app/common/domain/ConnectionService.ts
@@ -1,5 +1,5 @@
 import { UserCredentialsDTO, User } from '@/common/domain/User';
 
 export interface ConnectionService {
-  login(userCredentialDTO: UserCredentialsDTO): Promise<User>;
+  login(userCredentialDTO: UserCredentialsDTO): Promise<string>;
 }
--- src/main/webapp/app/common/secondary/ConnectionRepository.ts
+++ src/main/webapp/app/common/secondary/ConnectionRepository.ts
@@ -5,14 +5,14 @@ import { ConnectionService } from '@/common/domain/ConnectionService';
 export default class ConnectionRepository implements ConnectionService {
   constructor(private axiosHttp: AxiosHttp) {}
 
-  async login(userCredentialDTO: UserCredentialsDTO): Promise<User> {
+  async login(userCredentialDTO: UserCredentialsDTO): Promise<string> {
     return await this.axiosHttp.post<any, UserCredentialsDTO>('/api/authenticate', userCredentialDTO).then(result => {
       const bearerToken = result.headers.authorization;
       if (bearerToken && bearerToken.slice(0, 7) === 'Bearer ') {
         const jwt = bearerToken.slice(7, bearerToken.length);
-        return new User(userCredentialDTO.username, result.data.id, jwt);
+        return jwt;
       } else {
-        return new User();
+        return '';
       }
     });
   }
