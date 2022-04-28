6ba60966f82304f5e1206dd7306fdbefd738438a
RIOUAL Matthieu <60732084+matthieulapatate@users.noreply.github.com>
2022-04-28 14:55:56 +0200

    Update src/test/javascript/spec/common/domain/JWTStoreService.spec.ts
    Co-authored-by: Andrieux Séraphin <seraphin.andrieux@outlook.fr>

--- src/test/javascript/spec/common/domain/JWTStoreService.spec.ts
+++ src/test/javascript/spec/common/domain/JWTStoreService.spec.ts
@@ -13,7 +13,7 @@ describe('Test JWT store', () => {
 
   it('test set token', () => {
     store.setToken(TOKEN);
-    expect(store.token).toStrictEqual(
+    expect(store.token).toStrictEqual(TOKEN
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
     );
   });
