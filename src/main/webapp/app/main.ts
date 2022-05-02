5323e6c60a40131898304e0c4c6a1eeb66fb2cad
matthieulapatate <m.rioual83@gmail.com>
2022-05-02 02:29:57 +0200

    testing every routes

--- src/main/webapp/app/main.ts
+++ src/main/webapp/app/main.ts
@@ -6,22 +6,20 @@ import piniaPersist from 'pinia-plugin-persist';
 import AuthenticationRepository from './common/secondary/AuthenticationRepository';
 import { AxiosHttp } from './http/AxiosHttp';
 import axios from 'axios';
-import { jwtStore } from '@/common/domain/JWTStoreService';
 import ConsoleLogger from './common/secondary/ConsoleLogger';
 
 // jhipster-needle-main-ts-import
 
 const axiosHttp = new AxiosHttp(axios.create({ baseURL: '' }));
-const connectionRepository = new AuthenticationRepository(axiosHttp);
+const authenticationRepository = new AuthenticationRepository(axiosHttp);
 const consoleLogger = new ConsoleLogger(console);
 
 const app = createApp(App);
 const pinia = createPinia();
-const store = jwtStore();
-const router = createRouter(store);
 pinia.use(piniaPersist);
 app.use(pinia);
-app.provide('connectionService', connectionRepository);
+const router = createRouter();
+app.provide('authenticationService', authenticationRepository);
 app.provide('logger', consoleLogger);
 app.use(router);
 // jhipster-needle-main-ts-provider
