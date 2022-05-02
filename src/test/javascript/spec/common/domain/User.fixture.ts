90a2cbac42029ad41bd20964807fae65e626621a
matthieulapatate <m.rioual83@gmail.com>
2022-05-02 09:00:05 +0200

    Remove User class no used

--- src/test/javascript/spec/common/domain/User.fixture.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '@/common/domain/User';
-
-export const createUser = (user?: Partial<User>): User => ({
-  username: 'randomUSername',
-  id: '1234567890',
-  role: 'ADMIN',
-  ...user,
-});
